ac0897967d09c1e502526aa6d10232c57b0622b4
GerCassa <gercassa@gmail.com>
2018-09-05 13:30:44 +0100

    findByTitle added and working

--- models/cinema.js
+++ models/cinema.js
@@ -2,10 +2,17 @@ const Cinema = function (films) {
   this.films = films;
 };
 
-Cinema.prototype.findTitle = function () {
+Cinema.prototype.findAllTitles = function () {
   return this.films.map( (film) => {
     return film.title;
   });
+};
+
+Cinema.prototype.findByTitle = function (title) {
+  const result = this.films.filter((film) => {
+    return film.title === title;
+  });
+  return result[0];
 };
 
 module.exports = Cinema;
